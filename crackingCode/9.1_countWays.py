ca31a31348f8e4d9b45192333c864703452b4b3c
Jiyoung Kim <sweetherb0411@hotmail.com>
2017-05-06 11:22:49 +0900

    update

--- crackingCode/9.1_countWays.py
+++ crackingCode/9.1_countWays.py
@@ -1,7 +1,13 @@
-# Question] A child going up a staircase with n steps,
-# can hop up 1,2 or 3 steps at a time. How many ways can the child reach the top?
-# Dynamic Programming 이용
-# O(3^n)
+'''
+*Question
+A child going up a staircase with n steps,
+can hop up 1,2 or 3 steps at a time. How many ways can the child reach the top?
+
+*Idea
+Dynamic Programming (Recursion)
+
+Time Complexity : O(3^n)
+'''
 
 def countWays(n):
     if (n<0):
--- crackingCode/9.4_findSubsets.py
+++ crackingCode/9.4_findSubsets.py
@@ -1,13 +1,26 @@
-# sub_sets([1,2])
-# =findSubset([],[1,2])
-# =findSubset([],[2]) + findSubset([1],[2])
-# =findSubset([],none) + findSubset([2],none) + findSubset([1],none) + findsubSet([1,2],none)
-# =[]+[2]+[1]+[1,2]
-# =[ [],[2],[1],[1,2]]
+'''
+*Idea
+Recursion
 
-#python 메소드
-# 1) prev = [2,1], cur =3 일때
-# prev+[cur] -> [2,1,3]
+sub_sets([1,2])
+=findSubset([],[1,2])
+=findSubset([],[2]) + findSubset([1],[2])
+=findSubset([],none) + findSubset([2],none) + findSubset([1],none) + findsubSet([1,2],none)
+=[]+[2]+[1]+[1,2]
+=[ [],[2],[1],[1,2] ]
+
+sub_sets([1,2,3])
+=findSubset([],[1,2,3])
+=findSubset([],[2,3]) + findSubset([1],[2,3])
+=findSubset([],[3])+findSubset([2],[3])+ findSubset([1],[3]) + findSubset([1,2],[3])
+=findSubset([],none) + findSubset([3],none) + findSubset([2],none) + findSubset([2,3],none) +findSubset([1],none) + findSubset([1,3],none) +findsubSet([1,2],none) +findsubSet([1,2,3],none)
+=[]+[3]+[2]+[2,3]+[1]+[1,3]+[1,2]+[1,2,3]
+=[ [],[1],[2],[3],[1,2],[1,3],[2,3],[1,2,3] ]
+
+*Python
+1) When prev = [2,1], cur =3,
+prev+[cur] -> [2,1] + [3] = [2,1,3]
+'''
 
 class find_subsets_from_set:
     def sub_sets(self,set):
